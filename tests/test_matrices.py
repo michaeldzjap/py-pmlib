048f48f93ddc1ee7674f281c3672fe40d8b6a33c
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2023-11-29 13:50:35 +0100

    Add second difference matrix tests

--- tests/test_matrices.py
+++ tests/test_matrices.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_array_equal
 from pmlib.utils.matrices import second_difference_matrix
 
 
-def test_second_difference_matrix() -> None:
+def test_second_difference_matrix_coefficients() -> None:
     """
     Assert that the second difference matrix contains the expected
     coefficients at all relevant indices.
@@ -14,3 +14,14 @@ def test_second_difference_matrix() -> None:
     matrix = second_difference_matrix()
 
     assert_array_equal(array([-2, -2, -2]), matrix.A.diagonal(0))
+    assert_array_equal(array([1, 1]), matrix.A.diagonal(1))
+    assert_array_equal(array([1, 1]), matrix.A.diagonal(-1))
+
+
+def test_second_difference_matrix_shape() -> None:
+    """
+    Assert that the second difference matrix has the expected shape.
+    """
+    matrix = second_difference_matrix(4)
+
+    assert (4, 4) == matrix.shape
